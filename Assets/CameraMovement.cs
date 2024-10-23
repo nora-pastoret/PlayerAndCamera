f8c6c95106234fd271d6e7ce05a1024a068e3d68
nora-pastoret <nora.pastoret@estudiantat.upc.edu>
2024-10-23 10:13:42 +0200

    movementcamarawip

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
+    //CamaraController _camaracontroller; //preguntar charachter controler si ho podem aplicar camara
+    InputController _input;
+
+    public float Speed = 1;
+
     void Start()
     {
         
+        _input = GetComponent<InputController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Look();
+    }
+
+    private void Look()
+    {
+        var localInput =  _input.Look.x ; //aixo funcionara amb el ratoli??
+      //buscar rotate amb y vector3 amb la y
+        //transform.Rotate();
     }
 }
